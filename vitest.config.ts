02c7d75288c637e877a18d809fd68ac4bcfccd5a
Ahmed Alatawi <aalat001@gmail.com>
2024-11-20 11:54:53 -0500

    Added timeout

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
+    testTimeout: 10000,
     setupFiles: ["./src/setupTests.ts"],
     include: ["**/*.{test,spec}.{ts,tsx}"],
     coverage: {
